de5edea50a6ceb818434229bd92d81e1d73d6c54
Steven Kingston <skingston.dev@gmail.com>
2018-12-16 14:44:40 +0000

    Ammends the structure of the hex grid
    Adds hover state and selection functionality

--- src/App.js
+++ src/App.js
@@ -3,21 +3,49 @@ import Hexagon from "./components/Hexagon";
 
 import "./App.scss";
 
-const hexes = [
-  { type: "Book", colour: "red" },
-  { type: "Online Course", colour: "green" },
-  { type: "Article", colour: "blue" },
-  { type: "Mentor", colour: "yellow" },
-  { type: "Course", colour: "pink" }
-];
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hexes: [
+        { type: "Book", colour: "red", active: false },
+        { type: "Online Course", colour: "green", active: false },
+        { type: "Article", colour: "blue", active: false },
+        { type: "Mentor", colour: "orange", active: false },
+        { type: "Course", colour: "pink", active: false },
+        { type: "Book", colour: "red", active: false },
+        { type: "Online Course", colour: "green", active: false },
+        { type: "Article", colour: "blue", active: false },
+        { type: "Mentor", colour: "orange", active: false },
+        { type: "Course", colour: "pink", active: false }
+      ]
+    };
+  }
+
+  handleHexClick = type => {
+    const { hexes } = this.state;
+    const newHexes = hexes.map(hex => {
+      if (hex.type === type) {
+        hex.active = true;
+      }
+      return hex;
+    });
+    this.setState({ hexes: newHexes });
+  };
+
   render() {
+    const { hexes } = this.state;
     return (
       <div className="App">
         <div className="Hexgrid">
-          {hexes.map(hex => (
-            <Hexagon colour={hex.colour} text={hex.type} />
+          {hexes.map((hex, index) => (
+            <Hexagon
+              key={index}
+              colour={hex.colour}
+              text={hex.type}
+              active={hex.active}
+              handleClick={this.handleHexClick}
+            />
           ))}
         </div>
       </div>
--- src/components/Hexagon/index.jsx
+++ src/components/Hexagon/index.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "./styles.scss";
 
-const Hexagon = ({ colour, text }) => (
-  <div className={`Hexagon Hexagon--${colour}`}>
-    <p className="Hexagon__text">{text}</p>
+const Hexagon = ({ colour, text, active = false, handleClick }) => (
+  <div
+    onClick={() => handleClick(text)}
+    className={`Hexagon Hexagon--${colour}`}
+  >
+    <p className="Hexagon__text">{`${text} ${active ? "- active" : ""}`}</p>
   </div>
 );
 
